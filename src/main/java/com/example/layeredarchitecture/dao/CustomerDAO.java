b2c6c1807e271e68228eeadde2c59ed7e71d437f
L.K.H.M Lakdiw <manuthlakdiv2006@gmail.com>
2025-01-20 13:23:58 +0530

    added customer crud operations to the interface

--- src/main/java/com/example/layeredarchitecture/dao/CustomerDAO.java
+++ src/main/java/com/example/layeredarchitecture/dao/CustomerDAO.java
@@ -1,5 +1,22 @@
 package com.example.layeredarchitecture.dao;
 
+import com.example.layeredarchitecture.model.CustomerDTO;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public interface CustomerDAO {
+
+    public ArrayList<CustomerDTO> loadAllCustomers() throws SQLException, ClassNotFoundException;
+
+    public void saveCustomer(CustomerDTO customerDTO) throws SQLException, ClassNotFoundException;
+
+    public void updateCustomer(CustomerDTO customerDTO) throws SQLException, ClassNotFoundException;
+
+    public boolean existCustomer(String id) throws SQLException, ClassNotFoundException;
+
+    public void deleteCustomer(String id) throws SQLException, ClassNotFoundException;
+
+    public String generateNewCustomerID() throws SQLException, ClassNotFoundException;
 
 }
