0518aaa78838e2ff27a919e86085b525290c763e
L.K.H.M Lakdiw <manuthlakdiv2006@gmail.com>
2025-01-20 19:02:36 +0530

    apply existCustomer method

--- src/main/java/com/example/layeredarchitecture/controller/PlaceOrderFormController.java
+++ src/main/java/com/example/layeredarchitecture/controller/PlaceOrderFormController.java
@@ -193,10 +193,8 @@ public class PlaceOrderFormController {
     }
 
     boolean existCustomer(String id) throws SQLException, ClassNotFoundException {
-        Connection connection = DBConnection.getDbConnection().getConnection();
-        PreparedStatement pstm = connection.prepareStatement("SELECT id FROM Customer WHERE id=?");
-        pstm.setString(1, id);
-        return pstm.executeQuery().next();
+        ItemDAO itemDAO = new ItemDAOImpl();
+        return itemDAO.existsItem(id);
     }
 
     public String generateNewOrderId() {
